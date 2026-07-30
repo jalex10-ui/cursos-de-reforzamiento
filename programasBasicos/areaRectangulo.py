3d358c5abecf513862f11156275424ed78f964c2
Jordan <jalexreyes0710@gmail.com>
2026-07-30 15:18:19 -0600

    Primer commit

---
+++ programasBasicos/areaRectangulo.py
@@ -0,0 +1,5 @@
+# Programa para calcular el area de un rectangulo
+base = float(input("Ingresa la base: "))
+altura = float(input("Ingresa la altura: "))
+area = base * altura
+print("El area del rectanguloes:", area)
