07eeca5038df127eee0f77ede7bc29c639c2aadb
Jordan <jalexreyes0710@gmail.com>
2026-07-30 21:54:13 -0600

    factorial.py

---
+++ programasBasicos/factorial.py
@@ -0,0 +1,9 @@
+# Factorial
+num = int(input("Numero para factorial: "))
+factorial = 1
+if num < 0:
+    print("Factorial no definido para negativos")
+else:
+    for i in range(1, num + 1):
+        factorial *= i
+    print("El factorial de", num, "es:", factorial)
